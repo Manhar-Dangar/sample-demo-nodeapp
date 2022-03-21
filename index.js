1c9f0f5d6a3f87d21a63b8853b504c64fb9a8e81
Manhar-Dangar <73153726+Manhar-Dangar@users.noreply.github.com>
2022-03-21 14:19:02 +0530

    add

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node! i am edittttt\n ....I am adding as a new feature..........DDDDDDDDDDDXXXXXXXXXXXXXXX'
+  const msg = 'Hello Node!'
   res.end(msg);
 });
 
